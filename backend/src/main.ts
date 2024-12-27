327a56f39c1aacc37d680b3b948d16148d4939c7
erictsai <eric.tsai.hm@gmail.com>
2024-12-27 20:30:08 +0800

    update backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,11 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.setGlobalPrefix('api/v1');
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
