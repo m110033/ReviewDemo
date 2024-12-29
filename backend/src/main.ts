32b5504777dd0f70f2f0d26517690253584b110f
erictsai <eric.tsai.hm@gmail.com>
2024-12-29 19:21:10 +0800

    update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,18 +2,26 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/_next', express.static(join(__dirname, '../frontend/.next')));
+  app.use('/', express.static(join(__dirname, '../frontend/out')));
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: process.env.FRONTEND_ORIGIN || 'http://localhost:3000',
+    methods: process.env.CORS_METHODS || 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
   app.setGlobalPrefix('api/v1');
   const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
-  await app.listen(process.env.PORT ?? 3000);
+  const port = parseInt(process.env.PORT, 10) || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
